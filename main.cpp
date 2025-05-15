6ce9105546d332330c71e7e3f78823d4264de1da
yuumuu <haidar.alhazen93@gmail.com>
2025-05-15 19:07:33 +0700

    Menambahkan Method hapus pada DoubleLinkedList - main

--- main.cpp
+++ main.cpp
@@ -72,6 +72,19 @@ class DoubleLinkedList {
 
             current->next = newNode; // Step 9d: current.next = newNode
         }
+
+        void hapus() {
+            if (START == NULL) {
+                cout << "\nList is empty" << endl;
+                return;
+            }
+
+            cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+            int rollNo;
+            cin >> rollNo;
+
+            Node *current = START;
+        }
 };
 
 int main() {
