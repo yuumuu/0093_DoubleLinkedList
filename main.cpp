f2082d48fffba4e4f201bb8e1a6635be37e49de8
yuumuu <haidar.alhazen93@gmail.com>
2025-05-15 20:42:34 +0700

    Membuat Menu pada main - main

--- main.cpp
+++ main.cpp
@@ -189,5 +189,18 @@ int main() {
     DoubleLinkedList list;
     char choice;
 
+    do {
+        cout << "\nMenu:\n";
+        cout << "1. Add Record\n";
+        cout << "2. Delete Record\n";
+        cout << "3. View Ascending\n";
+        cout << "4. View Descending\n";
+        cout << "5. Search Record\n";
+        cout << "6. Exit\n";
+        cout << "Enter your choice: ";
+
+        cin >> choice;
+    } while (choice != '6');
+
     return 0;
 }
